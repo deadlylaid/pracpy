6dc364724e1ff040d0589eb3f4e13c30c1be2fd6
deadlylaid <deadlylaid@gmail.com>
2017-07-11 00:58:54 +0900

    update : decorator.py

--- decorator.py
+++ decorator.py
@@ -4,6 +4,15 @@ import datetime
 import time
 
 
+def my_food(original_function):
+    food = '탕수육'
+
+    @wraps(original_function)
+    def wrapper(*args, **kwargs):
+        print('자 모든 {}함수가 실행을 끝냈으니 이제 {}를 먹으러가자'.format(original_function.__name__, food))
+        return original_function(*args, **kwargs)
+    return wrapper
+
 def my_logger(original_function):
     import logging
     logging.basicConfig(filename='{}.log'.format(original_function.__name__), level=logging.INFO)
@@ -30,10 +39,11 @@ def my_timer(original_function):
 
     return wrapper
 
+@my_food
 @my_timer
 @my_logger
 def display_info(name, age):
     time.sleep(1)
     print('display_info({}, {}) 가 실행되었습니다. '.format(name, age))
 
-display_info('10년뒤 한민수', 36)
+display_info('한민수', 26)
