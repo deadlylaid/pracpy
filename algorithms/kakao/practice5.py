a35a79e0e66b151c1c3fcec7ddaee96fc44abc22
deadlylaid <deadlylaid@gmail.com>
2017-09-15 21:39:02 +0900

    update : kakao practice5.py

--- algorithms/kakao/practice5.py
+++ algorithms/kakao/practice5.py
@@ -1,8 +1,16 @@
 def solution(land):
-    result=[]
-    for i in len(land):
-        for j in range(4):
-
+    len_list = len(land)
+    dp = [ [0,0,0,0] for _ in range(len_list) ]
+    for i in range(len_list):
+        if i == 0:
+            dp[0][0] = land[0][0]
+            dp[0][1] = land[0][1]
+            dp[0][2] = land[0][2]
+            dp[0][3] = land[0][3]
+        else:
+            dp[i][0] = max(dp[i-1][1], dp[i-1][2], dp[i-1][3]) + land[i][0]
+            dp[i][1] = max(dp[i-1][0], dp[i-1][2], dp[i-1][3]) + land[i][1]
+            dp[i][2] = max(dp[i-1][0], dp[i-1][1], dp[i-1][3]) + land[i][2]
+            dp[i][3] = max(dp[i-1][0], dp[i-1][1], dp[i-1][2]) + land[i][3]
+    answer = max(dp[i][0], dp[i][1], dp[i][2], dp[i][3])
     return answer
-
-solution([[1,2,3,5],[5,6,7,8],[4,3,2,1]])
