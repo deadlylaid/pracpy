8374777c0a875118cf6e1f11f967ea81aca3b2d9
deadlylaid <deadlylaid@gmail.com>
2017-12-03 20:57:19 +0900

    update: 이진트리 순회 구현

--- algorithms/data-structure/binary_tree.py
+++ algorithms/data-structure/binary_tree.py
@@ -4,3 +4,40 @@ class TreeNode:
         self.value = value
         self.left = left
         self.right = right
+
+def print_tree(node):
+    """
+    구현해볼것
+    """
+
+
+def preorder(node):
+    """
+    왼쪽 - 중간 - 오른쪽
+    """
+
+    print(node.value, end=" ")
+    if node.left:
+        preorder(node.left)
+    if node.right:
+        preorder(node.right)
+
+def inorder(node):
+    """
+    중간 - 왼쪽 - 오른쪽
+    """
+    if node.left:
+        inorder(node.left)
+    print(node.value, end=" ")
+    if node.right:
+        inorder(node.right)
+
+def postorder(node):
+    """
+    왼쪽 - 오른쪽 - 중간
+    """
+    if node.left:
+        postorder(node.left)
+    if node.right:
+        postorder(node.right)
+    print(node.value, end=" ")
