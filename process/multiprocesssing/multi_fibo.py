7c838b8ac61a1ee7bfc9897e85f6e7ee1c98aef4
deadlylaid <deadlylaid@gmail.com>
2018-09-29 21:06:34 +0900

    multiprocess

---
+++ process/multiprocesssing/multi_fibo.py
@@ -0,0 +1,29 @@
+from multiprocessing import Pool
+import time
+
+start_time = time.time()
+
+result_list = []
+
+def fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+#result_list = []
+#
+#for num in num_list:
+#    result_list.append(fibo(num))
+def return_fibo(n):
+    return fibo(n)
+
+num_list = [31, 32, 33, 34]
+
+pool = Pool(processes=4)
+print(list(map(int, pool.map(return_fibo, num_list))))
+
+print("--- %s seconds ---" % (time.time() - start_time))
+
